411e1a02b2a708bdfc4ab630feff31762af101bb
Mark Hakansson <mark.hakansson@live.se>
2021-03-02 13:56:01 +0100

    Fetch object addresses from DWARF

--- src/replay/mod.rs
+++ src/replay/mod.rs
@@ -1,4 +1,7 @@
+pub mod dwarf;
+
 use crate::cli::Analyze;
+use gimli::read::{AttributeValue, EvaluationResult, Location};
 use object::{Object, ObjectSection};
 use std::path::PathBuf;
 use std::process::{Command, ExitStatus};
@@ -13,13 +16,28 @@ pub fn analyze(a: Analyze) {
     let mut cargo_path = project_dir.clone();
     target_dir.push("target/");
     cargo_path.push("Cargo.toml");
-
-    build_replay_harness(&a, &mut cargo_path, &mut target_dir)
-        .expect("Could not build the replay harness");
-
+    target_dir.push("thumbv7em-none-eabi/debug/test-harness");
     println!("{:?}", target_dir);
-    dwarf_check(target_dir);
+    let objects = dwarf::get_replay_addresses(target_dir);
+    println!("{:#x?}", objects.unwrap());
 }
+
+// pub fn analyze(a: Analyze) {
+//     let project_dir = match a.path.clone() {
+//         Some(path) => path,
+//         None => PathBuf::from("./"),
+//     };
+//     let mut target_dir = project_dir.clone();
+//     let mut cargo_path = project_dir.clone();
+//     target_dir.push("target/");
+//     cargo_path.push("Cargo.toml");
+//
+//     build_replay_harness(&a, &mut cargo_path, &mut target_dir)
+//         .expect("Could not build the replay harness");
+//
+//     println!("{:?}", target_dir);
+//     dwarf_check(target_dir);
+// }
 
 fn build_replay_harness(
     a: &Analyze,
@@ -57,68 +75,4 @@ fn build_replay_harness(
         .args(&["--manifest-path", cargo_path.to_str().unwrap()]);
 
     cargo.status()
-}
-
-fn dwarf_check(path: PathBuf) {
-    let file = fs::File::open(&path).unwrap();
-    let mmap = unsafe { memmap::Mmap::map(&file).unwrap() };
-    let object = object::File::parse(&*mmap).unwrap();
-    let endian = if object.is_little_endian() {
-        gimli::RunTimeEndian::Little
-    } else {
-        gimli::RunTimeEndian::Big
-    };
-    dump_file(&object, endian).unwrap();
-}
-
-fn dump_file(object: &object::File, endian: gimli::RunTimeEndian) -> Result<(), gimli::Error> {
-    // Load a section and return as `Cow<[u8]>`.
-    let load_section = |id: gimli::SectionId| -> Result<borrow::Cow<[u8]>, gimli::Error> {
-        match object.section_by_name(id.name()) {
-            Some(ref section) => Ok(section
-                .uncompressed_data()
-                .unwrap_or(borrow::Cow::Borrowed(&[][..]))),
-            None => Ok(borrow::Cow::Borrowed(&[][..])),
-        }
-    };
-    // Load a supplementary section. We don't have a supplementary object file,
-    // so always return an empty slice.
-    let load_section_sup = |_| Ok(borrow::Cow::Borrowed(&[][..]));
-
-    // Load all of the sections.
-    let dwarf_cow = gimli::Dwarf::load(&load_section, &load_section_sup)?;
-
-    // Borrow a `Cow<[u8]>` to create an `EndianSlice`.
-    let borrow_section: &dyn for<'a> Fn(
-        &'a borrow::Cow<[u8]>,
-    ) -> gimli::EndianSlice<'a, gimli::RunTimeEndian> =
-        &|section| gimli::EndianSlice::new(&*section, endian);
-
-    // Create `EndianSlice`s for all of the sections.
-    let dwarf = dwarf_cow.borrow(&borrow_section);
-
-    // Iterate over the compilation units.
-    let mut iter = dwarf.units();
-    while let Some(header) = iter.next()? {
-        println!(
-            "Unit at <.debug_info+0x{:x}>",
-            header.offset().as_debug_info_offset().unwrap().0
-        );
-        let unit = dwarf.unit(header)?;
-
-        // Iterate over the Debugging Information Entries (DIEs) in the unit.
-        let mut depth = 0;
-        let mut entries = unit.entries();
-        while let Some((delta_depth, entry)) = entries.next_dfs()? {
-            depth += delta_depth;
-            println!("<{}><{:x}> {}", depth, entry.offset().0, entry.tag());
-
-            // Iterate over the attributes in the DIE.
-            let mut attrs = entry.attrs();
-            while let Some(attr) = attrs.next()? {
-                println!("   {}: {:?}", attr.name(), attr.value());
-            }
-        }
-    }
-    Ok(())
 }
